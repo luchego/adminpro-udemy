03c73d4540cd040f5d5bb774bca5e91df4ac7cb0
Luis Castillo <luis_castle_2011@hotmail.com>
2020-05-21 09:08:58 -0500

    Primer Commit

---
+++ src/app/shared/breadscrumbs/breadscrumbs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadscrumbs',
+  templateUrl: './breadscrumbs.component.html',
+  styles: [
+  ]
+})
+export class BreadscrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
