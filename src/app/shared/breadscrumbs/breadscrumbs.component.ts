c451491485aa0104ebad9e5997fcb1697ba9bf78
Luis Castillo <luis_castle_2011@hotmail.com>
2020-05-24 07:20:54 -0500

    Sección 8 terminada

--- src/app/shared/breadscrumbs/breadscrumbs.component.ts
+++ src/app/shared/breadscrumbs/breadscrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadscrumbs',
@@ -7,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BreadscrumbsComponent implements OnInit {
+  titulo: string;
+  constructor(private router: Router,
+              private title: Title,
+              private meta: Meta) {
+    this.getDataRoute()
+    .subscribe( data => {
+      console.log(data);
+      this.titulo = data.titulo;
+      this.title.setTitle(this.titulo);
 
-  constructor() { }
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
+      this.meta.updateTag(metaTag);
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter( evento => evento instanceof ActivationEnd),
+      filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map( (evento: ActivationEnd) => evento.snapshot.data)
+    );
+  }
 }
